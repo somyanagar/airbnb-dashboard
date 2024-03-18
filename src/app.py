17f1ba19d44d1d4093040ef888c89c4b2924c847
Han Chen <ch2023@student.ubc.ca>
2024-03-18 10:22:18 -0700

    changes

--- src/app.py
+++ src/app.py
@@ -3,10 +3,10 @@ from dash import Dash, dcc, html
 import dash_bootstrap_components as dbc
 import os
 # --------------------------------APP-------------------------
-
-
-# pages_folder = os.getcwd() + '/apps'
-app=Dash(__name__, use_pages=True, pages_folder='/apps', external_stylesheets=[dbc.themes.JOURNAL])
+pages_folder = os.getcwd() + '/apps'
+app=Dash(__name__, use_pages=True, pages_folder=pages_folder, external_stylesheets=[dbc.themes.JOURNAL])
+# # pages_folder = os.getcwd() + '/apps'
+# app=Dash(__name__, use_pages=True, pages_folder='/apps', external_stylesheets=[dbc.themes.JOURNAL])
 server=app.server
 # the style arguments for the sidebar. We use position:fixed and a fixed width
 SIDEBAR_STYLE = {
